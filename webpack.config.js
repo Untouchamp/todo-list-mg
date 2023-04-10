65db8ad608804b957eae2f71a66fb7fc0fd11358
David Kuchukhidze <d.kuchukhidze@quality-lab.ru>
2023-04-10 21:53:55 +0300

    webpack initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: "./src/index.tsx",
+
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react", ["@babel/preset-typescript", {
+                            "allowNamespaces": true
+                        }]]
+                    }
+                }
+            },
+        ]
+    },
+
+    resolve: {
+        extensions: ["*", ".js", ".jsx", ".ts", ".tsx", ".scss", ".css"],
+    }
+};
