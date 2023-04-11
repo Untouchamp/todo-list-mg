a4aa134d424c2ce301ab6ad30c15df9f0dbaf2c8
David Kuchukhidze <d.kuchukhidze@quality-lab.ru>
2023-04-11 20:15:52 +0300

    Husky pre commit added and prettier printWidth set to 80

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  trailingComma: 'es5',
-  tabWidth: 4,
-  singleQuote: true,
-  semi: false,
-  singeAttributePerLine: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    singleQuote: true,
+    semi: false,
+    singeAttributePerLine: true,
+    printWidth: 80,
 }
